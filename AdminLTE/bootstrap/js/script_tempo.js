4ea68b9a3ee94ddf3f61e5965d8fdca51798ef86
JeanCardoso <jeancardoso@Jarvis.local>
2016-07-21 17:31:43 -0300

    Correção de bugs e melhorias

--- AdminLTE/bootstrap/js/script_tempo.js
+++ AdminLTE/bootstrap/js/script_tempo.js
@@ -9,24 +9,33 @@
             //$('#poster').html("<h2 class='loading'>Ha! We haven't forgotten to validate the form! Please enter something.</h2>");
 
          } else {
-
-           // $('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
-
             $.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + nomeCidade + '&mode=json&units=metric&cnt=7&APPID=42c1148f010111d8cafc7598f3b40baf', function(data){
 				
 				console.log(data);
 				
+				var cont;
+				var maiorTemp = 0;
+				var menorTemp = 9999;
+				var diaMaiorTemp = new Date();
+				var diaMenorTemp = new Date();
 				// data.list     é o array com o clima dos dias
+				for (cont = 0; cont < (data.list).length; cont++){
+					if (data.list[cont].temp.max > maiorTemp) {
+						maiorTemp = data.list[cont].temp.max;
+						diaMaiorTemp = new Date(data.list[cont].dt * 1000);
+					} else if (data.list[cont].temp.min < menorTemp){
+						menorTemp = data.list[cont].temp.min;
+						diaMenorTemp = new Date(data.list[cont].dt * 1000);
+					}
+				}
 				
-				var day1 = data.list[0];
-				var primeiroDia = day1.temp.max;
-				var dataPrimeiroDia = new Date(day1.dt * 1000);
-				
-				console.log(dataPrimeiroDia);
-				
-				console.log("Vai fazer "+ primeiroDia +" C no primeiro dia.");
-				console.log("Referente ao dia "+ dataPrimeiroDia.getDate() + "/" + (dataPrimeiroDia.getMonth() + 1));
-				
+				$('#box1').html("<div='content_box1'> A maior temperatura será de "+ maiorTemp +"ºC, no dia "+ diaMaiorTemp.getDate() +"/"+ (diaMaiorTemp.getMonth() + 1) +"</p> <p>E a menor temperatura será de "+ menorTemp +"ºC, no dia "+ diaMenorTemp.getDate() +"/"+ (diaMenorTemp.getMonth() + 1) +"</p>");
+
+				console.log("A maior temperatura será "+ maiorTemp +" C.");
+				console.log("No dia "+ diaMaiorTemp.getDate() + "/" + (diaMaiorTemp.getMonth() + 1));
+				console.log("A menor temperatura será "+ menorTemp +" C.");
+				console.log("No dia "+ diaMenorTemp.getDate() + "/" + (diaMenorTemp.getMonth() + 1));
+
 			});
 			 
 			 
